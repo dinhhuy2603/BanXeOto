b3e552fc12305a45b76c8757920a454ab36fdea6
zzdaicazz <36125842+zzdaicazz@users.noreply.github.com>
2021-09-29 16:57:41 +0700

    Update header.php

--- wp-content/themes/banxeoto/header.php
+++ wp-content/themes/banxeoto/header.php
@@ -4,16 +4,16 @@
 	<meta charset="utf-8"/>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link href="https://fonts.googleapis.com/css?family=Roboto:300,300i,400,400i,500,500i,700,700i,900" rel="stylesheet">
-	<link rel="stylesheet" href="<?php bloginfo('template_directory'); ?> /css/bootstrap.min.css">
-	<link rel="stylesheet" href="<?php bloginfo('template_directory'); ?> /css/fontawesome-all.min.css">
-	<link rel="stylesheet" href="<?php bloginfo('template_directory'); ?> /css/slick.css">
-	<link rel="stylesheet" href="<?php bloginfo('template_directory'); ?> /css/slick-theme.css">
-	<link rel="stylesheet" href="<?php bloginfo('template_directory'); ?> /css/jquery.mmenu.css">
-	<link rel="stylesheet" href="<?php bloginfo('template_directory'); ?> /css/animate.css">
-	<link rel="stylesheet" href="<?php bloginfo('template_directory'); ?> /css/lightGallery.css" />
-	<link rel="stylesheet" href="<?php bloginfo('template_directory'); ?> /css/lightslider.css"/>
-	<link rel="stylesheet" href="<?php bloginfo('template_directory'); ?> /css/jquery.mmenu.positioning.css">
-	<link rel="stylesheet" href="<?php bloginfo('template_directory'); ?> /css/style.css">
+	<link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/css/bootstrap.min.css">
+	<link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/css/fontawesome-all.min.css">
+	<link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/css/slick.css">
+	<link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/css/slick-theme.css">
+	<link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/css/jquery.mmenu.css">
+	<link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/css/animate.css">
+	<link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/css/lightGallery.css" />
+	<link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/css/lightslider.css"/>
+	<link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/css/jquery.mmenu.positioning.css">
+	<link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/css/style.css">
 	<link rel="icon" href="<?php bloginfo('template_directory'); ?> /favicon.png" type="image/x-icon"/>
 	<?php wp_head(); ?>
 </head>
